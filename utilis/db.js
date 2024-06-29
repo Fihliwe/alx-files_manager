d669e02eba384df19649c5730e013534bb796b4c
Fihliwe <siyabulelmncube@gmail.com>
2024-06-29 17:26:56 +0000

    MongoDB utils

---
+++ utilis/db.js
@@ -0,0 +1,49 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.dbName = database;
+  }
+
+  async isAlive() {
+    try {
+      await this.client.connect();
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async nbUsers() {
+    try {
+      await this.client.connect();
+      const db = this.client.db(this.dbName);
+      const usersCollection = db.collection('users');
+      return await usersCollection.countDocuments();
+    } catch (error) {
+      console.error('Error counting users:', error);
+      return 0;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      await this.client.connect();
+      const db = this.client.db(this.dbName);
+      const filesCollection = db.collection('files');
+      return await filesCollection.countDocuments();
+    } catch (error) {
+      console.error('Error counting files:', error);
+      return 0;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
