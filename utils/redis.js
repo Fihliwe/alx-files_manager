1621655972da97a86dfe7406952060fe0b19c15c
Fihliwe <siyabulelmncube@gmail.com>
2024-07-01 10:06:06 +0000

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,37 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
+const { promisify } = require('util');
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (err) => console.log(err));
+        this.connected = false;
+        this.client.on('connect', () => {
+            this.connected = true;
+        });
+    }
+
+    isAlive() {
+        return this.connected;
+    }
+
+    async get(key) {
+        const getAsync = promisify(this.client.set).bind(this.client);
+        return await getAsync(key);
+    }
+    
+
+    async set(key, val, dur) {
+        const setAsync = promisify(this.client.set).bind(this.client);
+        await setAsync(key, val, 'EX', dur);
+    }
+
+    async del(key) {
+        const delAsync = promisify(this.client.del).bind(this.client);
+        await delAsync(key);
+    }
+}
+
+    const redisClient = new RedisClient();
+
+    module.exports = redisClient;
